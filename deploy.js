bc8920a392241d99249c9344c4111fd0f64a7797
Saad Selim <saadselim6@gmail.com>
2022-07-22 07:52:44 -0400

    first commit

---
+++ deploy.js
@@ -0,0 +1,24 @@
+// synchronoous [solidity]
+// asynchronous [javascript]
+
+// cooking
+
+// Synchronous
+// 1. put popcorn in microwave
+// 2. Wait for poopcoorn to finish
+// 3. Pour drinks for everyone
+
+
+// Asynchronous
+// 1. Put popcorn in the micrwave
+// 2. Pour drinks for everyone
+// 3. Wait for popcorn to finish
+
+
+
+function main() {
+  console.log("hi");
+  let variable = 5;
+  console.log(variable);
+}
+main();
